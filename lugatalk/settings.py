7f3153bf90f6f23fa8d18c95384565ecdad1bf83
Edgarkimbugwe <kimbuny@gmail.com>
2024-06-02 20:15:46 +0000

    Edited CSRF trusted origins for local development

--- lugatalk/settings.py
+++ lugatalk/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9v^st(r3l*kdi=h*36ff%5(-826t5-v1l!9k^@0mi0!gc_01un'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-edgarkimbugwe-lugatalk-nkn7k2rlz18.ws-eu114.gitpod.io',
@@ -93,7 +93,7 @@ DATABASES = {
 }
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://https://8000-edgarkimbugwe-lugatalk-nkn7k2rlz18.ws-eu114.gitpod.io/",
+    "https://8000-edgarkimbugwe-lugatalk-nkn7k2rlz18.ws-eu114.gitpod.io",
     "https://https://lugatalk-ab90580d7f17.herokuapp.com/"
 ]
 
