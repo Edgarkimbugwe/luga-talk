6330745c1beac8e2dd69d727f9c7be2c591c7f1c
Edgarkimbugwe <kimbuny@gmail.com>
2024-06-02 21:17:16 +0000

    updates on  the post model

--- luga/models.py
+++ luga/models.py
@@ -5,11 +5,11 @@ from django.contrib.auth.models import User
 STATUS = ((0, "Draft"), (1, "Published"))
 
 
-class Post(models.Model):
+class BlogPost(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts"
+        User, on_delete=models.CASCADE, related_name="luga_posts"
     )
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
