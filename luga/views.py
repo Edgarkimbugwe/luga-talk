f44878b14e293fe7ef186f8f69d00a3c2a123ec6
Edgarkimbugwe <kimbuny@gmail.com>
2024-06-03 21:21:14 +0000

    added a filter to the PostList queryset to display only posts that have been set to status published

--- luga/views.py
+++ luga/views.py
@@ -4,5 +4,5 @@ from .models import BlogPost
 
 # Create your views here.
 class PostList(generic.ListView):
-    queryset = BlogPost.objects.all()
+    queryset = BlogPost.objects..filter(status=1)
     template_name = "blogpost_list.html"
