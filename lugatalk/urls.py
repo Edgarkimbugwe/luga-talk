ff48c0de40e61cb1f8e75fabbf13280be996a079
Edgarkimbugwe <kimbuny@gmail.com>
2024-06-03 10:43:54 +0000

    edited the url path that connects to luga app with a name attribute

--- lugatalk/urls.py
+++ lugatalk/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('luga.urls')),
+    path('', include('luga.urls'), name='luga-urls'),
     path('admin/', admin.site.urls),
 ]
