9d6e47eb92309ab787bcaddcf6a58c43f9e1aade
Edgarkimbugwe <kimbuny@gmail.com>
2024-06-05 00:11:27 +0000

    created a template with navbar, main and footer that will be used to inherit other templates
    created and index page to be used to view the existing posts created in the admin
    
    added a css file in static to be used to apply styles to the blog post page

--- luga/views.py
+++ luga/views.py
@@ -5,4 +5,10 @@ from .models import BlogPost
 # Create your views here.
 class PostList(generic.ListView):
     queryset = BlogPost.objects.filter(status=1)
-    template_name = "blogpost_list.html"
+    template_name = "luga/index.html"
+    paginate_by = 6
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['post_list'] = context['object_list']
+        return context
