099856f11fcfd9369281d5f39f8ddc95e7af5b16
Edgarkimbugwe <kimbuny@gmail.com>
2024-06-08 11:49:18 +0000

    added runtime.txt to specify the exact Python version number to use in Heroku,
    debug set to false for deployment

--- lugatalk/settings.py
+++ lugatalk/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'django-insecure-9v^st(r3l*kdi=h*36ff%5(-826t5-v1l!9k^@0mi0!gc_01un'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-edgarkimbugwe-lugatalk-nkn7k2rlz18.ws-eu114.gitpod.io',
