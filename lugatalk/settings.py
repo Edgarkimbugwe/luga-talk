c052e3c6417433f59a0564ac0698e81a5adb2659
Edgarkimbugwe <kimbuny@gmail.com>
2024-06-14 15:16:46 +0000

    installed crispy-bootstrap5 packages to display the register form in the browser

--- lugatalk/settings.py
+++ lugatalk/settings.py
@@ -41,12 +41,13 @@ ALLOWED_HOSTS = [
 INSTALLED_APPS = [
     'django.contrib.admin',
     'users.apps.UsersConfig',
-    'crispy_forms',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'django_summernote',
     'luga',
 ]
