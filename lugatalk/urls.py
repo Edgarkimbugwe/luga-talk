a50ed2693a3784276c2e0ef51a47052ddc72574b
Edgarkimbugwe <kimbuny@gmail.com>
2024-06-14 17:42:16 +0000

    imported Django auth_views and added url path for login and logout using class based views

--- lugatalk/urls.py
+++ lugatalk/urls.py
@@ -2,12 +2,15 @@
 URL configuration for lugatalk project.
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('summernote/', include('django_summernote.urls')),
-    path('', include('luga.urls'), name='luga-urls'),
+    path('', include('luga.urls'), name='luga-urls'), 
 ]
