ada063ba705648ffdcf1d20e40a3ab5968c4c025
Edgarkimbugwe <kimbuny@gmail.com>
2024-06-14 20:47:32 +0000

    Added an import for the login required decorator to restrict access to views to only authenticated users
    Added a login URL in the setting.py file to display the login route and profile page when user is logged in
    
    Added an import to the urls.py for django to access and use the settings defined in the project

--- lugatalk/urls.py
+++ lugatalk/urls.py
@@ -4,6 +4,8 @@ URL configuration for lugatalk project.
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from users import views as user_views
 
 urlpatterns = [
