ee7cfc1ec2ba50f840638709cbff8f38030cdcff
Edgarkimbugwe <kimbuny@gmail.com>
2024-06-15 09:07:04 +0000

    Import Profile model in forms.py
    Add UserUpdateForm to update user details
    
    Add ProfileUpdateForm to update profile image

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,9 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
-
+# Fileds that show when user opts to register for an account
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
 
@@ -16,3 +17,18 @@ class UserRegisterForm(UserCreationForm):
             'password1',
             'password2'
             ]
+
+# update form with specific fileds that the user
+# can update on the profile page
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image',]
