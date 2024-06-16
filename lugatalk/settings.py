d38772dcc8c7879ff736e26815f637d4be7f230d
Edgarkimbugwe <kimbuny@gmail.com>
2024-06-16 18:25:42 +0000

    reinstalled cloudinary and added it to requirements as there was an error in the previous download
    setup cloudinary in the models.py file and setting of the project

--- lugatalk/settings.py
+++ lugatalk/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'django-insecure-9v^st(r3l*kdi=h*36ff%5(-826t5-v1l!9k^@0mi0!gc_01un'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-edgarkimbugwe-lugatalk-nkn7k2rlz18.ws-eu114.gitpod.io',
@@ -46,9 +46,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'crispy_forms',
     'crispy_bootstrap5',
     'django_summernote',
+    'cloudinary',
     'luga',
 ]
 
