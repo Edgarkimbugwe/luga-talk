4b441c34e409d509ca0b3046462051cc935a6f78
Edgarkimbugwe <kimbuny@gmail.com>
2024-06-16 14:33:06 +0000

    Fixed comment edit functionality and improve code formatting by;
    correcting the comment_edit view to properly update comments by fixing the instance parameter in the CommentForm initialization.
    
    Ensuring comments are set as unapproved upon editing.
    
    Improving readability and consistency in code formatting for all views.
    
    Integrated JavaScript within the blogpost_detail.html template to handle comment editing.
    
    Added edit buttons for comments visible to the comment author.
    
    Ensured the edit functionality correctly sets the form action to the appropriate URL.
    
    Improved user experience by dynamically updating form content for comment editing.
    
    Added jQuery library to base template for enhanced Javascript support to ensure comaptibility with the existing Bootstrap 4 setup

--- luga/views.py
+++ luga/views.py
@@ -61,22 +61,22 @@ def comment_edit(request, slug, comment_id):
     """
     if request.method == "POST":
 
-        queryset = Post.objects.filter(status=1)
-        post = get_object_or_404(queryset, slug=slug)
+        queryset = BlogPost.objects.filter(status=1)
+        blogpost = get_object_or_404(queryset, slug=slug)
         comment = get_object_or_404(Comment, pk=comment_id)
         comment_form = CommentForm(data=request.POST, instance=comment)
 
         if comment_form.is_valid() and comment.author == request.user:
             comment = comment_form.save(commit=False)
-            comment.post = post
+            comment.post = blogpost
             comment.approved = False
             comment.save()
-            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+            messages.add_message(request, messages.SUCCESS, 'Comment has been updated! successfully')
         else:
             messages.add_message(request, messages.ERROR,
                                  'Error updating comment!')
 
-    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+    return HttpResponseRedirect(reverse('blogpost_detail', args=[slug]))
 
 
 class AboutView(TemplateView):
--- static/js/comments.js
+++ static/js/comments.js
@@ -1,11 +1,8 @@
 const editButtons = document.getElementsByClassName("btn-edit");
-const commentText = document.getElementById("id_body");
+const commentText = document.getElementById("id_text");
 const commentForm = document.getElementById("commentForm");
 const submitButton = document.getElementById("submitButton");
-
-const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
-const deleteButtons = document.getElementsByClassName("btn-delete");
-const deleteConfirm = document.getElementById("deleteConfirm");
+const slug = "{{ blogpost.slug }}";
 
 // Attach click event listeners to all edit buttons
 for (let button of editButtons) {
@@ -14,11 +11,13 @@ for (let button of editButtons) {
       let commentId = e.target.getAttribute("data-comment_id");
       // Get the current content of the comment using its ID
       let commentContent = document.getElementById(`comment${commentId}`).innerText;
+
+
       // Populate the comment text area with the current comment content
       commentText.value = commentContent;
       // Change the submit button text to indicate updating action
       submitButton.innerText = "Update";
       // Set the form action to the edit comment URL with the comment ID
-      commentForm.setAttribute("action", `edit_comment/${commentId}`);
+      commentForm.setAttribute("action", `/${slug}/edit_comment/${commentId}`);
     });
   }
