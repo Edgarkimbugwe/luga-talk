9afc2cd78f065f73ff183f01e3c444667046d796
Edgarkimbugwe <kimbuny@gmail.com>
2024-06-16 09:19:04 +0000

    created a path in the luga urls.py file that will point to the comment_edit view
    Added a comment_edit view to handle editing comments on blog posts
    
    comment_edit view validates form and ensures that comment author matches the current user
    
    updates commentand mark it as unapproved until reviewed
    
    The view displays success or error messages and redirects to post detail page after processing

--- luga/urls.py
+++ luga/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('about/', AboutView.as_view(), name='about'),
     path('<slug:slug>/', views.blogpost_detail, name='blogpost_detail'),
+    path('<slug:slug>/edit_comment/<int:comment_id>',
+         views.comment_edit, name='comment_edit'),
 ]
