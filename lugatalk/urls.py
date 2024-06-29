457eb16a6a8ba4e0043419da3a2094d04965f131
Edgar Kimbugwe <kimbuny@gmail.com>
2024-06-29 12:51:27 +0000

    Imported the custom 404 handler and set it in the urls.py file with in the project folder

--- lugatalk/urls.py
+++ lugatalk/urls.py
@@ -7,6 +7,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from users import views as user_views
+from luga.views import custom_404
 
 from users.views import delete_account
 
@@ -23,3 +24,6 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Custom error handlers
+handler404 = custom_404
