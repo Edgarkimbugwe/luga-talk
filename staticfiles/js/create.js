af3d155b2e2027942016e2e1d57e14d81dc20bdf
Edgar Kimbugwe <kimbuny@gmail.com>
2024-07-02 22:24:09 +0000

    collected statis files for heroku

---
+++ staticfiles/js/create.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.summernote').summernote({
+        height: 300,                 // set editor height
+        minHeight: null,             // set minimum height of editor
+        maxHeight: null,             // set maximum height of editor
+        focus: true,                 // set focus to editable area after initializing summernote
+        width: '100%'                // set width to 100% to ensure responsiveness
+    });
+});
