176ba36b5c67464615aaaa54ae0bffcbcdfcd634
Edgar Kimbugwe <kimbuny@gmail.com>
2024-07-07 15:27:06 +0000

    Fix profile image display issue using CloudinaryField

--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ from cloudinary.models import CloudinaryField
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.webp', upload_to='profile_pics')
+    image = CloudinaryField('image', default='default_y57ejb', blank=True)
 
     def __str__(self):
         return f'{self.user.username} Profile'
