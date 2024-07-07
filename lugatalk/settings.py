942ced903bb8ef6fc086b6853428d3df6239df65
Edgar Kimbugwe <kimbuny@gmail.com>
2024-07-07 20:18:56 +0000

    Added links to the index and users blog post pages to increase on the users ease at accessing parts of the page.

--- lugatalk/settings.py
+++ lugatalk/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-edgarkimbugwe-lugatalk-j66uhktsk84.ws.codeinstitute-ide.net',
